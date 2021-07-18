b430916d65eff0d8d807ba3bec522524c48f8fd6
gowtham4950@gmail.com <gowtham4950@gmail.com>
2021-07-18 17:05:12 +0530

    Initial setup for execute learned topics main concepts 1 - 5 and 12

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
-
+// import Clock from './Clock/Clock';
+// import HelloWorld from './HelloWorld/HelloWorld';
+// import TODoApp from './TODO/ToDoApp';
+import GowthamSearchFilter from './ThinkingInReact/GowthamSearchFilter';
 function App() {
+  const PRODUCTS = [
+    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+  ];
+
+  const element = (
+    // Hello World Example
+    // <HelloWorld name="Gowtham"/>
+
+    // TODO List Example
+    //       <div>
+    //           <TODoApp />
+    //       </div>
+
+    // Clock Example
+    // <div>
+    //   <Clock />
+    //   <Clock />
+    //   <Clock />
+    // </div>
+
+    // Thinking in React
+    <div>
+      <GowthamSearchFilter products={PRODUCTS}/>
+    </div>
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {element}
     </div>
   );
 }
